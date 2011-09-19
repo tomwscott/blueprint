be7af1bd659e9e411f83a757be24f99e144605c8
Tom Scott <tomwscott@gmail.com>
2011-09-19 19:36:45 +0100

    Tom: Creating default MVC3 layout

--- src/Blueprint.Web/Global.asax.cs
+++ src/Blueprint.Web/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Blueprint.Web
@@ -24,7 +20,7 @@ namespace Blueprint.Web
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Default", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
